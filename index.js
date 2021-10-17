868338a5a2ca86aaf191484e5578b190917ed650
Nelly Turicenco <turicenco@gmail.com>
2021-10-17 21:04:53 +0300

    updated styles using bootstrap5. created form with validation and toast element on submit

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+const toastTrigger = document.getElementById("liveToastBtn");
+const toastLiveExample = document.getElementById("liveToast");
+if (toastTrigger) {
+  toastTrigger.addEventListener("click", function () {
+    var toast = new bootstrap.Toast(toastLiveExample);
+
+    toast.show();
+  });
+}
